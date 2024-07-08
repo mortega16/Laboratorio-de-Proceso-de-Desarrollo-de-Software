93a12592cc5b8d8eebbb15d4574bbaf79f09c853
mortega16 <mortegad@ucenfotec.ac.cr>
2024-07-07 23:32:37 -0600

    Se crea inicio de sesion aedmas registro de usuarios y API

---
+++ AdministradorTareas/WebApp/wwwroot/js/ControlActions.js
@@ -0,0 +1,201 @@
+﻿function ControlActions() {
+	this.URL_API = "https://localhost:7196/api/";
+
+	this.GetUrlApiService = function (service) {
+		return this.URL_API + service;
+	}
+
+	this.GetTableColumsDataName = function (tableId) {
+		var val = $('#' + tableId).attr("ColumnsDataName");
+
+		return val;
+	}
+
+	this.FillTable = function (service, tableId, refresh) {
+
+		if (!refresh) {
+			columns = this.GetTableColumsDataName(tableId).split(',');
+			var arrayColumnsData = [];
+
+
+			$.each(columns, function (index, value) {
+				var obj = {};
+				obj.data = value;
+				arrayColumnsData.push(obj);
+			});
+			$('#' + tableId).DataTable({
+				"processing": true,
+				"ajax": {
+					"url": this.GetUrlApiService(service),
+					dataSrc: ''
+				},
+				"columns": arrayColumnsData
+			});
+		} else {
+			$('#' + tableId).DataTable().ajax.reload();
+		}
+
+	}
+
+	this.GetSelectedRow = function () {
+		var data = sessionStorage.getItem(tableId + '_selected');
+
+		return data;
+	};
+
+	this.BindFields = function (formId, data) {
+		console.log(data);
+		$('#' + formId + ' *').filter(':input').each(function (input) {
+			var columnDataName = $(this).attr("ColumnDataName");
+			this.value = data[columnDataName];
+		});
+	}
+
+	this.GetDataForm = function (formId) {
+		var data = {};
+
+		$('#' + formId + ' *').filter(':input').each(function (input) {
+			var columnDataName = $(this).attr("ColumnDataName");
+			data[columnDataName] = this.value;
+		});
+
+		console.log(data);
+		return data;
+	}
+
+
+
+	this.PostToAPI = function (service, data, callBackFunction) {
+
+		$.ajax({
+			type: "POST",
+			url: this.GetUrlApiService(service),
+			data: JSON.stringify(data),
+			contentType: "application/json; charset=utf-8",
+			dataType: "json",
+			success: function (data) {
+				if (callBackFunction) {
+					Swal.fire(
+						'Correcto!',
+						
+					)
+					callBackFunction(data);
+				}
+			},
+			error: function (jqXHR, textStatus, errorThrown) {
+
+				var responseJson = jqXHR.responseJSON;
+				var message = jqXHR.responseText;
+
+				if (responseJson) {
+					var errors = responseJson.errors;
+					var errorMessages = Object.values(errors).flat();
+					message = errorMessages.join("<br/> ");
+				}
+				Swal.fire({
+					icon: 'error',
+					title: 'Oops...',
+					html: message,
+					
+				})
+			}
+		});
+	};
+
+
+	this.PutToAPI = function (service, data, callBackFunction) {
+		var jqxhr = $.put(this.GetUrlApiService(service), data, function (response) {
+			var ctrlActions = new ControlActions();
+
+			Swal.fire(
+				'Good job!',
+				'Transaction completed!',
+				'success'
+			)
+
+			if (callBackFunction) {
+				callBackFunction(response);
+			}
+
+		})
+			.fail(function (response) {
+				var data = response.responseJSON;
+				var errors = data.errors;
+				var errorMessages = Object.values(errors).flat();
+				message = errorMessages.join("<br/> ");
+				Swal.fire({
+					icon: 'error',
+					title: 'Oops...',
+					html: message,
+					footer: 'UCenfotec'
+				})
+			})
+	};
+
+	this.DeleteToAPI = function (service, data, callBackFunction) {
+		var jqxhr = $.delete(this.GetUrlApiService(service), data, function (response) {
+			var ctrlActions = new ControlActions();
+			Swal.fire(
+				'Good job!',
+				'Transaction completed!',
+				'success'
+			)
+
+			if (callBackFunction) {
+				callBackFunction(response);
+			}
+		})
+			.fail(function (response) {
+				var data = response.responseJSON;
+				var errors = data.errors;
+				var errorMessages = Object.values(errors).flat();
+				message = errorMessages.join("<br/> ");
+				Swal.fire({
+					icon: 'error',
+					title: 'Oops...',
+					html: message,
+					footer: 'UCenfotec'
+				})
+			})
+	};
+
+	this.GetToApi = function (service, callBackFunction) {
+		var jqxhr = $.get(this.GetUrlApiService(service), function (response) {
+			console.log("Response " + response);
+			if (callBackFunction) {
+				callBackFunction(response);
+			}
+
+		});
+	}
+}
+
+$.put = function (url, data, callback) {
+	if ($.isFunction(data)) {
+		type = type || callback,
+			callback = data,
+			data = {}
+	}
+	return $.ajax({
+		url: url,
+		type: 'PUT',
+		success: callback,
+		data: JSON.stringify(data),
+		contentType: 'application/json'
+	});
+}
+
+$.delete = function (url, data, callback) {
+	if ($.isFunction(data)) {
+		type = type || callback,
+			callback = data,
+			data = {}
+	}
+	return $.ajax({
+		url: url,
+		type: 'DELETE',
+		success: callback,
+		data: JSON.stringify(data),
+		contentType: 'application/json'
+	});
+}
---
+++ AdministradorTareas/WebApp/wwwroot/js/pages/Usuarios.js
@@ -0,0 +1,122 @@
+﻿function UsuarioController() {
+    this.ViewName = "Usuarios";
+    this.ApiService = "Usuario";
+
+    //Metodo a ejecutar al incio de la vista
+    this.InitView = function () {
+        console.log("User view init!!");
+
+        //Bind del click de boton create con la funcion correpondiente
+
+        $("#btnRegistrar").click(function () {
+            var vc = new UsuarioController();
+            vc.Create();
+        })
+        $("#btnUpdate").click(function () {
+            var vc = new UsuarioController();
+            vc.Update();
+        })
+        $("#btnInicioSesion").click(function () {
+            var vc = new UsuarioController();
+            vc.InicioSesion();
+        })
+
+
+    }
+
+    this.Create = function () {
+        var usuario = {};
+        usuario.id = $("#cedulaUsuario").val();
+        usuario.correo = $("#correoUsuario").val();
+        usuario.nombre = $("#nombreUsuario").val();
+        usuario.apellido1 = $("#apellido1Usuario").val();
+        usuario.apellido2 = $("#apellido2Usuario").val();
+        usuario.contrasenna = $("#usuarioContraseña").val();
+
+
+        var ca = new ControlActions();
+
+        var serviceRoute = this.ApiService + "/Create";
+        ca.PostToAPI(serviceRoute, usuario, function () {
+            console.log("Usuario creado-->" + JSON.stringify(usuario));
+        });
+    }
+
+
+    this.VerificarCrendenciales = function (correo,contrasenna) {
+
+        var ca = new ControlActions();
+
+        var urlService = ca.GetUrlApiService("Usuario/VerificarCredenciales");
+
+        $.ajax({
+            type: "GET",
+            url: urlService + "?correo=" + correo + "&contrasenna=" + contrasenna,
+
+            success: function (usuario) {
+                Swal.fire(
+                    'Se ha iniciado sesion correctamente!',
+
+                )
+                setTimeout(function () {
+                    window.location.href = "https://localhost:7103/";
+                }, 2000);
+
+            },
+
+            error: function (xhr, status, error) {
+                Swal.fire({
+					icon: 'error',
+                    title: 'Credenciales invalidos',
+                })
+                setTimeout(function () {
+                    window.location.href = "https://localhost:7103/InicioSesion";
+                }, 2000);
+            }
+        });
+
+       
+    }
+
+    this.InicioSesion = function () {
+        var usuario = {
+            correo: $("#correoUsuario").val(),
+            contraseña: $("#usuarioContraseña").val()
+        };
+
+        var vs = new UsuarioController();
+        vs.VerificarCrendenciales(usuario.correo, usuario.contraseña)
+
+     
+  
+    };
+
+
+
+
+
+    this.Update = function () {
+        var user = {};
+        user.id = $("#UserId").val();
+        user.name = $("#UserName").val();
+        user.email = $("#UserEmail").val();
+        user.department = $("#UserDepartment").val();
+        user.role = $("#UserRole").val();
+        user.birthdate = $("#UserBirthdate").val();
+
+
+        var ca = new ControlActions();
+
+        var serviceRoute = this.ApiService + "/Update";
+        ca.PutToAPI(serviceRoute, user, function () {
+            console.log("User update-->" + JSON.stringify(user));
+        });
+    }
+
+}
+
+
+$(document).ready(function () {
+    var vc = new UsuarioController();
+    vc.InitView();
+})
