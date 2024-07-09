783c58048920da6275aada381f4887f7b4bca187
mortega16 <mortegad@ucenfotec.ac.cr>
2024-07-09 10:28:23 -0600

    cambios para que muestre la tabla

--- AdministradorTareas/WebApp/wwwroot/js/pages/tarea.js
+++ AdministradorTareas/WebApp/wwwroot/js/pages/tarea.js
@@ -1,5 +1,4 @@
-﻿var tc = new TareaController();
-var tc;
+﻿
 
 function TareaController() {
     this.ViewName = "Tareas";
@@ -16,39 +15,23 @@ function TareaController() {
         var ca = new ControlActions();
         var urlService = ca.GetUrlApiService(this.ApiService + "/SELECCIONAR_TAREA_POR_USUARIO?user=117487545");
 
-        $("#tblTarea").
-        ({
-            "processing": true,
-            "serverSide": false, // Change to true if you are doing server-side processing
+        var columns = [];
+        columns[0] = { "data": "titulo" }
+        columns[1] = { "data": "descripcion" }
+        columns[2] = { "data": "prioridad" }
+        columns[3] = { "data": "vencimiento" }
+        columns[4] = { "data": "id" }
+
+        $("#tblTarea").dataTable({
             "ajax": {
                 "url": urlService,
                 "dataSrc": ""
             },
-            "columns": [
-                { "data": "titulo" },
-                { "data": "descripcion" },
-                { "data": "prioridad" },
-                { "data": "vencimiento" },
-                { "data": "id" }
-            ]
-
+            "columns": columns
         });
-        $("#tblTarea tbody").on("click", "tr", function () {
-            // Extraer la fila a la que se le dio clic
-            var row = $(this).closest("tr");
 
-            // Extraer la data del registro contenido en la fila
-            var sedeData = $("#tblSede").DataTable().row(row).data();
-
-            // Mapear los valores del objeto de datos con el formulario
-            $("#idSede").val(sedeData.id);
-            $("#InputNombre").val(sedeData.nombre);
-            $("#InputDescripcion").val(sedeData.descripcion);
-            $("#InputUbicacion").val(sedeData.ubicacion);
-            $("#map-link").val(sedeData.link);
-
-        });
     };
+
 
 }
 
